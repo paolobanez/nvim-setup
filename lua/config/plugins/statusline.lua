7345575a0516a0ad2f0f01dc5a8d10cf387cebfa
Paolo Banez <paolobanez@gmail.com>
2026-05-28 14:08:46 +0800

    change theme

--- lua/config/plugins/statusline.lua
+++ lua/config/plugins/statusline.lua
@@ -4,7 +4,7 @@ return {
 
   config = function()
     require('lualine').setup({
-      theme = 'bamboo',
+      theme = 'auto',
       options = {
         icons_enabled = false,
         component_separators = '',
--- lua/config/plugins/theme.lua
+++ lua/config/plugins/theme.lua
@@ -1,26 +1,13 @@
 return {
-  "ribru17/bamboo.nvim",
+  "the-coding-doggo/batman.nvim",
   lazy = false,
   priority = 1000,
   opts = {
-    style = "vulgaris",
-    transparent = false,
-    dim_inactive = false,
-    term_colors = true,
-    ending_tildes = false,
-    code_style = {
-      comments = { italic = false },
-      conditionals = { italic = false },
-      keywords = {},
-      functions = {},
-      namespaces = { italic = false },
-      parameters = { italic = false },
-      strings = {},
-      variables = {},
-    },
+    theme = "classic",
+    use_persistence = false,
   },
   config = function(_, opts)
-    require("bamboo").setup(opts)
-    require("bamboo").load()
+    require("batman").setup(opts)
+    require("batman").load()
   end,
 }
